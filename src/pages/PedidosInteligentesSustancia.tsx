99ec4923535f433f515445903b94021afcaebcd8
ElNerdyyT <40747612+ElNerdyyT@users.noreply.github.com>
2025-07-05 17:23:08 -0700

    Update PedidosInteligentesSustancia.tsx

--- src/pages/PedidosInteligentesSustancia.tsx
+++ src/pages/PedidosInteligentesSustancia.tsx
@@ -1,1 +1,16 @@
+import { FunctionalComponent } from 'preact';
+import PageLayout from '../components/PageLayout';
+import PedidosInteligentesSustancia from '../components/PedidosInteligentesSustancia';
 
+const Kardex: FunctionalComponent = () => {
+  return (
+    <PageLayout
+      title="PedidosInteligentesSustancia"
+      description="Bienvenido al panel de pediddos. Aquí puedes ver los pedidos de cada sucursal."
+    >
+      <PedidosInteligentesSustancia />
+    </PageLayout>
+  );
+};
+
+export default Kardex;
