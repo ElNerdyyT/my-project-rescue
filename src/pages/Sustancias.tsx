245fa4c7e8df30b221670b43bb0843415119a014
ElNerdyyT <40747612+ElNerdyyT@users.noreply.github.com>
2025-07-08 10:27:56 -0700

    Update Sustancias.tsx

--- src/pages/Sustancias.tsx
+++ src/pages/Sustancias.tsx
@@ -1,1 +1,16 @@
+import { FunctionalComponent } from 'preact';
+import PageLayout from '../components/PageLayout';
+import sustanciasEditor from '../components/sustanciasEditor';
 
+const Ajustes: FunctionalComponent = () => {
+  return (
+    <PageLayout
+      title="Sustancias"
+      description="Bienvenido al panel sustancias."
+    >
+        <sustanciasEditor />
+    </PageLayout>
+  );
+};
+
+export default Sustancias;
